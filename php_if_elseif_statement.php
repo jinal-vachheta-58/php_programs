84eace86f8b20a17a7b9c5fe062fb0cfb7ba3dad
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2024-02-27 19:57:26 +0530

    php if elseif statement

---
+++ php_if_elseif_statement.php
@@ -0,0 +1,15 @@
+<?php
+    $x = 40;
+    $y = 50;
+
+    if($x > $y)
+    {
+        echo "x is larger";
+    }
+    elseif ($x < $y) {
+        echo "y is larger";
+    }
+    else {
+        echo "both are same";
+    }
+?>
